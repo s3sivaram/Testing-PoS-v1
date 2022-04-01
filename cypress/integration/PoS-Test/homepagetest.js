fb6633c4acb5c177146acd65e03825e7fe9d5594
Sivaram <s3sivaram@gmail.com>
2022-04-01 17:30:25 +0530

    itonly

--- cypress/integration/PoS-Test/homepagetest.js
+++ cypress/integration/PoS-Test/homepagetest.js
@@ -3,4 +3,13 @@ it("homepage", () => {
   cy.visit("https://jovial-khorana-553d04.netlify.app/");
   cy.get(".MuiGrid-container > :nth-child(1) > .MuiPaper-root").click();
   cy.get('.MuiTabs-flexContainer > [tabindex="-1"]').click();
+  cy.contains("Home").click();
+  cy.get("#combo-box-demo").click().type("{downarrow}{enter}");
+  cy.get('[data-testid="DeleteForeverIcon"] > path').click();
+});
+
+it.only("html test", () => {
+  cy.visit("http://127.0.0.1:5500/cypress/first.html");
+  cy.get("[type = 'text']").type("sivaram");
+  cy.get("button").click();
 });
